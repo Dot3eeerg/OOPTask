4e6bf866dea5dc68e1f66724e74e6be7817fd154
Dot3eeerg <91532031+Dot3eeerg@users.noreply.github.com>
2025-11-18 21:51:20 +0700

    Added functions implimentation

---
+++ Functions/PiecewiseLinearFunction.cs
@@ -0,0 +1,67 @@
+using System.Net.Http.Headers;
+using DataObjects;
+using Interfaces.Function;
+
+namespace Functions;
+
+sealed class PiecewiseLinearFunction : IParametricFunction<IDifferentialFunction>
+{
+    public IDifferentialFunction Bind(IVector parameters)
+    {
+        if (parameters.Count < 1)
+            throw new ArgumentException("List of coefficients cannot be empty");
+
+        return new InternalPiecewiseLinearFunction(parameters);
+    }
+    
+    private class InternalPiecewiseLinearFunction(IVector parameters) : IDifferentialFunction
+    {
+        public double Value(IVector point)
+        {
+            if (parameters.Count < 1)
+                throw new ArgumentException("Parameters are not bound");
+            if (point.Count != 1)
+                throw new ArgumentException("Cannot build polynomial function, dimensions must be 1");
+
+            var x = point[0];
+            var n = parameters.Count;
+            if (x <= 0) 
+                return parameters[0];
+            if (x >= n - 1)
+                return parameters[n - 1];
+            
+            var i = (int)Math.Floor(x);
+            var t = x - i;
+            return parameters[i] * (1 - t) + parameters[i + 1] * t;
+        }
+
+        public IVector Gradient(IVector point)
+        {
+            var x = parameters[0];
+            var n = parameters.Count;
+            var grad = new Vector
+            {
+                Capacity = n
+            };
+
+            if (x <= 0)
+            {
+                grad[0] = 1.0;
+                return grad;
+            }
+
+            if (x >= n - 1)
+            {
+                grad[n - 1] = 1.0;
+                return grad;
+            }
+            
+            var i = (int)Math.Floor(x);
+            var t = x - i;
+            grad[i] = 1 - t;
+            grad[i + 1] = t;
+
+            return grad;
+        }
+    }
+}
---
+++ Functions/PolynomialFunction.cs
@@ -0,0 +1,28 @@
+using DataObjects;
+using Interfaces.Function;
+
+namespace Functions;
+
+sealed class PolynomialFunction : IParametricFunction<IFunction>
+{
+    public IFunction Bind(IVector parameters)
+    {
+        if (parameters.Count < 1)
+            throw new ArgumentException("List of coefficients cannot be empty");
+        
+        return new InternalPolynomialFunction(parameters);
+    }
+
+    private class InternalPolynomialFunction(IVector parameters) : IFunction
+    {
+        public double Value(IVector point)
+        {
+            if (parameters.Count < 1)
+                throw new ArgumentException("Parameters are not bound");
+            if (point.Count != 1)
+                throw new ArgumentException("Cannot build polynomial function, dimensions must be 1");
+            
+            return parameters.Select((coeff, index) => coeff * Math.Pow(point.ElementAt(index), index)).Sum();
+        }
+    }
+}
